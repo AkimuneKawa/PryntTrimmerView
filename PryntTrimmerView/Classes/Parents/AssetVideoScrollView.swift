a82e8bd8e979bbf4181d8a05f778667b07c5f73b
Kawa AKimune <akimune1003@gmail.com>
2019-06-29 21:27:56 +0900

    サムネイルのバグ修正

--- PryntTrimmerView/Classes/Parents/AssetVideoScrollView.swift
+++ PryntTrimmerView/Classes/Parents/AssetVideoScrollView.swift
@@ -70,7 +70,7 @@ class AssetVideoScrollView: UIScrollView {
     private func getThumbnailFrameSize(from asset: AVAsset) -> CGSize? {
         guard let track = asset.tracks(withMediaType: AVMediaType.video).first else { return nil}
         
-        let assetSize = track.naturalSize.applying(track.preferredTransform)
+        let assetSize = track.naturalSize
         
         let height = frame.height
         let ratio = assetSize.width / assetSize.height
